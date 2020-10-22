6090af50d8b12205e2635d41337d2c3f6117c2b7
HubertRudowicz <71440489+HubertRudowicz@users.noreply.github.com>
2020-10-22 13:34:14 +0200

    Delete listing244.js

--- listing244.js
+++
@@ -1,8 +0,0 @@
-const pytanieiodpowiedz=[
-    ["Gdziekolwiek","Edward Stachura"],
-    ["Wyspa","Jonasz Kofta"],
-    ["Życie","Jan Twardowski"]
-];
-for(let i=0; i<pytanieiodpowiedz.length; i++){
-    console.log("Tytuł wiersza: "+ pytanieiodpowiedz[i][0]);
-}
