767a186718730120089f323f1e723c17a0ee7a27
HubertRudowicz <71440489+HubertRudowicz@users.noreply.github.com>
2020-10-22 13:34:54 +0200

    zadania z js

---
+++ listing244.js
@@ -0,0 +1,8 @@
+const pytanieiodpowiedz=[
+    ["Gdziekolwiek","Edward Stachura"],
+    ["Wyspa","Jonasz Kofta"],
+    ["Życie","Jan Twardowski"]
+];
+for(let i=0; i<pytanieiodpowiedz.length; i++){
+    console.log("Tytuł wiersza: "+ pytanieiodpowiedz[i][0]);
+}
